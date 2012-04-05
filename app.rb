e4140a625eb4db3dad0bc36a118a76d153ffedff
koshikawa <koshikawa@ppworks.jp>
2012-04-06 02:06:36 +0900

    Add basic auth for staging.

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'sinatra'
 require 'sass'
 require 'coffee_script'
 
+
+configure :staging do
+  use Rack::Auth::Basic do |username, password|
+    username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+  end
+end
+
 get '/' do
   erb :index
 end
